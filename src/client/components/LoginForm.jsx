8cba795eb9e7a546a610e206f202f4044528ed12
Engr.Sunday <asmelitus@gmail.com>
2018-05-21 17:49:35 +0300

    redux form setup

--- src/client/components/LoginForm.jsx
+++ src/client/components/LoginForm.jsx
@@ -1,48 +1,67 @@
-import { field, fieldArray } from 'redux-form';
+import React from 'react'
+import { Field, reduxForm, FieldArray } from 'redux-form';
 import isEmail from 'sane-email-validation';
+
+
 const validate = (values) => {
-  const errors = {}
+  const errors = {};
   if (!values.firstName) {
-    errors.firstName = 'Required'
+    errors.firstName = 'Required';
   }
   if (!values.lastName) {
-    errors.lastName = 'Required'
+    errors.lastName = 'Required';
   }
   if (!values.email) {
-    errors.email = 'Required'
-  } else if(!isEmail(value.email)) {
-    error.email = 'invalid email address'
+    errors.email = 'Required';
+  } else if (!isEmail(values.email)) {
+    errors.email = 'invalid email address';
   }
-  return errors
-}
+  if (values.phones && values.phones.length > 3) {
+    errors.phones = [];
+    errors.phones._error = 'Too many phones';
+  }
+  return errors;
+};
 
-const renderInput = field => {
+const renderInput = field => (
   <div>
-    <label>{field.input.placeholder}</label>
+    <label htmlFor={field.input.placeholder}>{field.input.placeholder}</label>
     <div>
       <input {...field.input} />
       {field.errors && field.touched && <span> {field.error}</span>}
     </div>
   </div>
-}
- const renderPhones = ({fields}) =>
- <ul>
-<li></li>
- </ul>
-const loginForm = ({ handleSubmit }) =>
+);
+
+const renderPhones = ({ fields }) => (
+  <ul>
+    <li><button type="button" onClick={() => fields.push()}>Add Button </button> </li>
+    {fields.map((field, index) =>
+      <li>
+        <Field name="field" component={renderInput} placeholder={'phone #$(index + 1)'} />
+        <button type="button" onClick={() => fields.remove(index)}> remove</button>
+      </li>
+    )}
+    {fields.errors && <li className="error">{fields.error}</li>}
+  </ul>
+);
+
+let LoginForm = ({ handleSubmit }) => (
   <div>
     <h1> LoginForm demo</h1>
     <form onSubmit={handleSubmit}>
-      <field name="firstName" component={renderInput} placeholder="firstName" />
-      <field name="lastName" component={renderInput} placeholder="lastName" />
-      <field name="email" component={renderInput} placeholder="email" type="email" />
-      <fieldArray name="phones" component={renderPhones} placeholder="email" type="email" />
+      <Field name="firstName" component={renderInput} placeholder="first Name" />
+      <Field name="lastName" component={renderInput} placeholder="last Name" />
+      <Field name="email" component={renderInput} placeholder="email" type="email" />
+      <FieldArray name="phones" component={renderPhones} placeholder="email" type="email" />
       <button type="submit">Submit</button>
     </form>
   </div>
-loginForm = reduxForm({
-  form: loginForm,
-  validate
-})(loginForm);
+  );
 
-export default loginForm;
+LoginForm = reduxForm({
+  form: 'loginForm',
+  validate
+})(LoginForm);
+
+export default LoginForm;
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,1 +1,14 @@
-// index folder
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+import store from './redux/store';
+
+// create redux store and set redux devtools
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>
+    , window.document.getElementById('app'));
