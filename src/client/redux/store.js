78f76441f183784dfcc3db8c3a3ba479504622c5
Engr.Sunday <asmelitus@gmail.com>
2018-08-21 15:33:49 +0100

    configured devtool and epic at the store.js

--- src/client/redux/store.js
+++ src/client/redux/store.js
@@ -1,7 +1,22 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { ReduxObservable } from './utils';
+import initialState from './initialState';
 import reducers from './rootReducers';
+import rootEpic from './rootEpic';
+import devToolsEnhancer from './devToolsEnhancer';  // eslint-disable-line no-unused-vars
 
+const middlewares = [
+  ReduxObservable.createEpicMiddleware(rootEpic),
+  routerMiddleware(browserHistory),
+];
 
-const store = createStore(reducers);
+const store = createStore(reducers, initialState, composeWithDevTools(
+  applyMiddleware(...middlewares),
+   devToolsEnhancer
+));
 
 export default store;
