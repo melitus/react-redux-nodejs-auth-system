b6c3fc3e5069ea02128bd20d7bf1212985381005
Engr.Sunday <asmelitus@gmail.com>
2018-09-26 23:48:06 +0100

    created crashrepoter util

--- src/client/redux/store/store.js
+++ src/client/redux/store/store.js
@@ -7,6 +7,7 @@ import { ReduxObservable } from '../utils';
 import initialState from './InitialStore/initialState';
 import reducers from './rootReducers';
 import rootEpic from './rootEpic';
+import crashReporter from '../../utils/sentry-redux-middleware';
 import devToolsEnhancer from './devToolsEnhancer'; // eslint-disable-line no-unused-vars
 
 const middlewares = [
@@ -17,7 +18,7 @@ const middlewares = [
 const store = createStore(
   reducers,
   initialState,
-  composeWithDevTools(applyMiddleware(...middlewares), devToolsEnhancer)
+  composeWithDevTools(applyMiddleware(...middlewares), devToolsEnhancer, crashReporter)
 );
 
 export default store;
